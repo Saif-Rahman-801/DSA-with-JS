61adfb230d2a2f1bc081d9d5dd0970833cb41544
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-18 20:40:12 +0600

    examples

--- Big_O.js
+++ Big_O.js
@@ -1,3 +1,4 @@
+// Exmple 01
 // without efficiancy
 const sumUptoOne = (n) => {
   let total = 0;
@@ -12,4 +13,25 @@ const sumUptoOne = (n) => {
 const sumUptoTwo = (n) => {
   return (n * (n + 1)) / 2;
 };
-console.log(sumUptoTwo(2));
+// console.log(sumUptoTwo(2));
+
+//example 02
+const shoutOutV1 = (n) => {
+  for (let i = 0; i < n; i++) {
+    console.log("What's up?");
+  }
+};
+const shoutOutV3 = (n) => {
+  let total = 0;
+  for (let i = 0; i < n; i++) {
+    console.log("What's up?");
+    return (total += i);
+  }
+}; //
+
+const shoutOutV2 = (n) => {
+  for (let i = 0; i < 4; i++) {
+    console.log("What's up?");
+  }
+};
+shoutOutV2(4);
